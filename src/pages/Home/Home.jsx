f1f852a511e77b87240b9711d3f11549c3470f74
Shriyash <ghimires1@my.smccd.edu>
2026-06-08 14:30:57 -0700

    feat: sperate default homepage using outlet

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,70 +1,7 @@
-import { Link, useOutletContext } from "react-router";
-import styles from "./Home.module.css";
-import clsx from "clsx";
-import { useState, useEffect, useContext } from "react";
-import { Loading } from "../../components/Loader";
-import ErrorPage from "../Error/Error";
-import { ShoppingCart } from "lucide-react";
-import Button from "../../components/Button/Button";
-import { useTheme } from "../../theme/ThemeContext";
-import { useFetch } from "../../hooks/useFetch";
+import { Outlet } from "react-router";
 
 function Home() {
-  const url = `https://dummyjson.com/products/category/fragrances`;
-  const { cart, setCart } = useOutletContext();
-  const { data, loading, error } = useFetch(url);
-  const { dark, toggle } = useTheme();
-
-  if (loading) {
-    return (
-      <div className={styles.loaderContainer}>
-        <Loading />
-      </div>
-    );
-  }
-  if (error) {
-    return <ErrorPage message={error} />;
-  }
-  const products = data?.products ?? [];
-
-  const addToCart = (productId) => {
-    const product = products.find((p) => p.id === productId);
-    let exists = cart.some((p) => p.id === productId);
-    if (!exists) {
-      setCart([...cart, { ...product, cartCount: 1 }]);
-    } else {
-      const newCart = cart.map((cartProduct) =>
-        cartProduct.id === productId
-          ? { ...cartProduct, cartCount: cartProduct.cartCount + 1 }
-          : cartProduct,
-      );
-
-      setCart(newCart);
-    }
-  };
-  return (
-    <section className={styles.homeContainer}>
-      <div className={styles.h2Contianer}>
-        <h2 className={styles.releaseH2}>Latest Releases</h2>
-      </div>
-      <div className={styles.newReleasegrid}>
-        {products.slice(1, 11).map((product) => {
-          return (
-            <div key={product.id} className={styles.newItemWrap}>
-              <article className={styles.CardContainer}>
-                <img src={product.images[0]} alt={product.title} />
-                <div className={styles.addCart}></div>
-                <div className={styles.cardInfo}>
-                  <p className={styles.rainbowPara}>{product.title}</p>
-                  <p>{`$${product.price}`}</p>
-                </div>
-              </article>
-            </div>
-          );
-        })}
-      </div>
-    </section>
-  );
+  return <Outlet />;
 }
 
 export default Home;
--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router";
 import Shop from "./Shop/Shop";
 import Home from "./Home/Home";
 import ErrorPage from "./Error/Error";
-
+import DefaultHome from "./Home/DefaultHome";
 // routes for the app | react-routing-library
 
 export const routes = [
@@ -13,8 +13,13 @@ export const routes = [
     errorElement: <ErrorPage />,
     children: [
       {
-        index: true,
         element: <Home />,
+        children: [
+          {
+            index: true,
+            element: <DefaultHome />,
+          },
+        ],
       },
       {
         path: "shop",
