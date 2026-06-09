1bc5fcee9ae254bd9f31a4082b681b5a729d2be4
Shriyash <ghimires1@my.smccd.edu>
2026-06-08 19:08:41 -0700

    fix: scrollview on product page

--- src/hooks/ScrollToTop.js
+++ src/hooks/ScrollToTop.js
@@ -5,7 +5,7 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useLayoutEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    window.scrollTo({ top: 70, left: 0, behavior: "smooth" });
   }, [pathname]);
 
   return null;
